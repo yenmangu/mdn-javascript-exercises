ed127cfe2f7eef50060632e38d59e8b9d9af527b
yenmangu <robert.shelford@googlemail.com>
2022-08-01 11:37:25 +0100

    saving

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    var repeatedString = "";
+    while (times > 0) {
+        repeatedString += string;
+        times--;
+    }
+    if (times < 0) {
+        return "ERROR"
+    } else {
+        return repeatedString;
+    }
 
-};
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//     var string = "";
 
+//     function toArray() {
+//         var newArray = string.split(string);
+//     }
+//     return newArray;
+
+//     function reverseArray() {
+//         var reversed = newArray.reverse()
+//     }
+//     return reversed;
+// };
+
+
+const reverseString = function(string) {
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
